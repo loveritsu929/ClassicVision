c9e85be34bddd73a4a1eaace608f136398459bfe
Chenxing <loveritsu929@gmail.com>
2019-12-11 16:29:00 -0500

    getDepth

--- 5/showImage.m
+++ 5/showImage.m
@@ -12,13 +12,13 @@ im2 = im2double(im2);
 darkImg1 = getDark(im1,3);
 darkImg2 = getDark(im2,3);
 
-J1 = dehaze(im1,darkImg1);
-J2 = dehaze(im2,darkImg2);
+J1 = dehaze(im1,1);
+J2 = dehaze(im2,1);
 
 %SUBPLOT('position',[left bottom width height])
 
 subplot('position', [0,0, 0.475, 1]);imshow(im1);title("(a1)");
 subplot('position', [0.5,0, 0.475, 1]);imshow(J1);title("(a2)");
-subplot('position', [0,0, 0.475, 1]);imshow(im2);title("(b1)");
-subplot('position', [0.5,0, 0.475, 1]);imshow(J2);title("(b2)");
+% subplot('position', [0,0, 0.475, 1]);imshow(im2);title("(b1)");
+% subplot('position', [0.5,0, 0.475, 1]);imshow(J2);title("(b2)");
 %subplot(1,3,3);imshow(J);title("de-haze image")
